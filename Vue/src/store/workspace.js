dc2faadc71d40b3bb8034058b79f7b291df5aaee
GeneralPoxter <generalpoxter@gmail.com>
2021-07-07 01:08:46 -0400

    Savable/loadable QAs

--- Vue/src/store/workspace.js
+++ Vue/src/store/workspace.js
@@ -4,10 +4,11 @@ export function defaultWorkspace(id) {
 		qas: [
 			{
 				id: 0,
-				title: "QA",
-				text: "",
+				title: 'QA',
+				text: ''
 			}
 		],
+		qa_index: 1,
 		qa_selected: 0,
 		widgets: [
 			{
@@ -15,11 +16,39 @@ export function defaultWorkspace(id) {
 				title: 'Timer',
 				type: 'Timer',
 				expanded: true,
-				maxHeight: '200px',
-			},
+				maxHeight: '200px'
+			}
 		],
-		widget_index: 1,
+		widget_index: 1
 	};
 }
 
+export function defaultQA(id) {
+	return {
+		id: id,
+		title: `QA (${id})`,
+		text: ''
+	}
+}
+
 export const initial_workspaces = [ defaultWorkspace(0) ];
+
+export function widgetTemplate(workspace, type) {
+	let widget_templates = {
+		Timer: {
+			id: workspace.widget_index,
+			title: 'Timer',
+			type: 'Timer',
+			expanded: true,
+			maxHeight: '200px',
+		},
+		Pronunciation: {
+			id: workspace.widget_index,
+			title: 'Pronunciation difficulty',
+			type: 'Pronunciation',
+			expanded: true,
+			maxHeight: '210px',
+		}
+	};
+	return widget_templates[type];
+}
