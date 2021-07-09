25f2ce5e599251c7aa17e0b49a747aca5db1f5a3
DCRepublic <damiancrene@gmial.com>
2021-07-09 16:29:51 -0400

    Latest Updates
    First implementation of pronunciation guide

---
+++ Flask/app/pronunciation.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import Blueprint, render_template, redirect
+from flask import Flask, jsonify, request
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+
+pronunciation = Blueprint('pronunciation', __name__)
+
+@pronunciation.route('/getpronunciation', methods=["POST"])
+def pronunciation():
+    if request.method == "POST":
+        question = request.form.get("text")
+
+    language = 'en'
+    myobj = gTTS(text=question, lang=language, slow=False)
+
+    myobj.save("pronunciation.mp3")
+    song = AudioSegment.from_mp3("pronunciation.mp3")
+
+    return(song)
+
+
+
+
+
+
+
