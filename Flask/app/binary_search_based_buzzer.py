fb726656ffef2336fe584625846cde3b650ee81d
Mr.Salt <972281745@qq.com>
2021-07-11 12:45:41 +0800

    Highlight

--- Flask/app/binary_search_based_buzzer.py
+++ Flask/app/binary_search_based_buzzer.py
@@ -1,48 +1,55 @@
 # Raj
+import warnings
+from importance import *
+from util import *
+from app import util, importance
+import sys
+from app import db, metadata
+from flask import Flask, jsonify, request
+from flask import Blueprint, render_template, redirect
+
+
 def warn(*args, **kwargs):
     pass
-import warnings
+
+
 warnings.warn = warn
-from flask import Blueprint, render_template, redirect
-from flask import Flask, jsonify, request
-from app import db, metadata
-import sys
 sys.path.append("..")
 sys.path.insert(0, './app')
 
-from app import util, importance
-from util import *
-from importance import *
 
 binary_search_based_buzzer = Blueprint('binary_search_based_buzzer', __name__)
 
-def buzz(question, min_index = 5):
-    
+
+def buzz(question, min_index=5):
+
     answer = []
     # temp_word_array = break_into_words(question)
     temp_word_array = question.split(' ')
     # check if buzzer ever goes above threshold
     index_of_bin_search = len(temp_word_array)
     question_sentence = question
-    temp_var = guess_top_n(question = [question_sentence], params = params, max = 3, n = 1)
-    if(temp_var[0][1]<threshold):
+    temp_var = guess_top_n(
+        question=[question_sentence], params=params, max=3, n=1)
+    if(temp_var[0][1] < threshold):
         return "Buzzer never crosses the threshold", False
     store_index = index_of_bin_search
-    max_index = index_of_bin_search -1
-    
+    max_index = index_of_bin_search - 1
+
     while max_index >= min_index:
-        index_of_bin_search =(max_index+min_index)//2
+        index_of_bin_search = (max_index+min_index)//2
         question_sentence = " ".join(temp_word_array[:index_of_bin_search])
-        temp_var = guess_top_n(question = [question_sentence], params = params, max = 3, n = 1)
-        if (temp_var[0][1]>threshold):
+        temp_var = guess_top_n(
+            question=[question_sentence], params=params, max=3, n=1)
+        if (temp_var[0][1] > threshold):
             max_index = index_of_bin_search-1
             store_index = index_of_bin_search
         else:
             min_index = index_of_bin_search+1
     # print("Index is :" + str(store_index) + " and the score is " + str(temp_var[0][1]) + " with guess = " + str(temp_var[0][0]) )
-    
-    
+
     return question_sentence, True
+
 
 @binary_search_based_buzzer.route("/buzz_full_question", methods=["POST"])
 def buzz_full_question():
@@ -57,11 +64,12 @@ def buzz_full_question():
     start = time.time()
     if(flag):
         importance_sentence = get_importance_of_each_sentence(buzzer_string)
-        buzzer_string = buzzer_string +" ||BUZZ||"
+        buzzer_string = buzzer_string + " ||BUZZ||"
     else:
+        print("question")
+        print(question)
         importance_sentence = get_importance_of_each_sentence(question)
     end = time.time()
     # print(end - start)
     print("----TIME (s) : /binary_search_based_buzzer/get_importance_sentence---", end - start)
-    return jsonify({"buzz": buzzer_string , "importance": importance_sentence})
-    
+    return jsonify({"buzz": buzzer_string, "importance": importance_sentence})
--- Flask/app/country_represent.py
+++ Flask/app/country_represent.py
@@ -1,5 +1,6 @@
+import pycountry
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity 
+from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 from nltk.corpus import stopwords
 import json
@@ -32,34 +33,41 @@ f_pop = open('app/query.json')
 wiki_population = json.load(f_pop)
 
 
-countries = list(map(lambda x: x['countryLabel'].lower() , wiki_population))
-population = list(map(lambda x: int(x['population']) , wiki_population))
+countries = list(map(lambda x: x['countryLabel'].lower(), wiki_population))
+population = list(map(lambda x: int(x['population']), wiki_population))
 
-import pycountry
 country_represent = Blueprint('country_represent', __name__)
 f_country = open('app/country.json')
 map_instance = json.load(f_country)
 total_instance = sum(map_instance.values())
+
+
 @country_represent.route("/country_present", methods=["POST"])
 def country_present():
     if request.method == "POST":
         question = request.form.get("text")
+    print("country")
     start = time.time()
-    message = ''    
+    message = ''
     # print(total_instance)
     under_countries = []
     over_countries = []
+    countries = []
     for country in map_instance.keys():
         if country in question.lower():
-            if len(list(filter(lambda x: x['countryLabel'].lower() == country.lower(), wiki_population))) !=0:
-                if map_instance[country.lower()]/total_instance < int(list(filter(lambda x: x['countryLabel'].lower() == country.lower(), wiki_population))[0]['population'])/sum(population): 
+            countries.append(country)
+            if len(list(filter(lambda x: x['countryLabel'].lower() == country.lower(), wiki_population))) != 0:
+                if map_instance[country.lower()]/total_instance < int(list(filter(lambda x: x['countryLabel'].lower() == country.lower(), wiki_population))[0]['population'])/sum(population):
                     under_countries.append(country)
                 else:
-                    over_countries.append(country) 
-    if len(under_countries) != 0: 
-        message = message + 'The country ' + ', '.join(under_countries) + ' in the question is/are from underrepresented group. The author will get 10 extra points. \n'
+                    over_countries.append(country)
+    if len(under_countries) != 0:
+        message = message + 'The country ' + \
+            ', '.join(under_countries) + \
+            ' in the question is/are from underrepresented group. The author will get 10 extra points. \n'
     else:
-         message = message + 'The country ' + ', '.join(over_countries) + ' in the question is/are from overrepresented group. The author can next time write question having underrepresented countries to earn extra points. \n'
+        message = message + 'The country ' + \
+            ', '.join(over_countries) + ' in the question is/are from overrepresented group. The author can next time write question having underrepresented countries to earn extra points. \n'
     end = time.time()
-    print("----TIME (s): /country_represent/country_present---",end - start)
-    return jsonify({"country_representation" : message})
+    print("----TIME (s): /country_represent/country_present---", end - start)
+    return jsonify({"country_representation": message, "country": countries})
