1ff0240e1b5be35f744d1c6502e4c11656c1cbb8
raj_shah <rajsanjayshah@gmail.com>
2021-07-12 01:47:24 +0530

    timer correct and workspace edit

--- Vue/src/store/workspace.js
+++ Vue/src/store/workspace.js
@@ -25,14 +25,15 @@ export function widgetTemplate(id, type) {
 			maxHeight: '300px'
 		},
 		SimilarQuestions: {
-			id: workspace.widget_index,
+			id: id,
 			title: 'Top5 similar questions',
 			type: 'SimilarQuestions',
 			expanded: true,
+			container: 'right',
 			maxHeight: '400px',
 		},
 		Buzzer: {
-			id: workspace.widget_index,
+			id: id,
 			title: 'Buzzer',
 			type: 'Buzzer',
 			container: 'left',
@@ -96,52 +97,3 @@ export function defaultQA(id) {
 
 export const initial_workspaces = [ defaultWorkspace(0) ];
 
-export function widgetTemplate(workspace, type) {
-	let widget_templates = {
-		Timer: {
-			id: workspace.widget_index,
-			title: 'Timer',
-			type: 'Timer',
-			container: 'left',
-			expanded: true,
-			maxHeight: '100px',
-		},
-		Pronunciation: {
-			id: workspace.widget_index,
-			title: 'Pronunciation difficulty',
-			type: 'Pronunciation',
-			container: 'left',
-			expanded: true,
-			maxHeight: '200px',
-		},
-		Representation: {
-			id: workspace.widget_index,
-			title: 'Suggestions',
-			type: 'Representation',
-			container: 'right',
-			expanded: true,
-			maxHeight: '300px',
-		},
-		SimilarQuestions: {
-			id: workspace.widget_index,
-			title: 'Top5 similar questions',
-			type: 'SimilarQuestions',
-			expanded: true,
-			container: 'right',
-			maxHeight: '400px',
-		},
-		Buzzer: {
-			id: workspace.widget_index,
-			title: 'Buzzer',
-			type: 'Buzzer',
-			container: 'left',
-			expanded: true,
-			maxHeight: '500px',
-		}
-	};
-	return widget_templates[type];
-}
-	
-
-
-export const initial_workspaces = [ defaultWorkspace(0) ];
