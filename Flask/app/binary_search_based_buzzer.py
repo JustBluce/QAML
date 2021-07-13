c9520081eba276af3b29a27e7bef2066330ab736
Mr.Salt <972281745@qq.com>
2021-07-13 18:31:42 +0800

    Highlight of buzzer

--- Flask/app/binary_search_based_buzzer.py
+++ Flask/app/binary_search_based_buzzer.py
@@ -1,4 +1,5 @@
 # Raj
+import re
 import warnings
 from importance import *
 from util import *
@@ -29,8 +30,9 @@ def buzz(question, min_index=5):
     # check if buzzer ever goes above threshold
     index_of_bin_search = len(temp_word_array)
     question_sentence = question
-    temp_var = guess_top_n(question = [question_sentence], params = params, max = 3, n = 1)
-    if(temp_var[0][1]<threshold_buzz):
+    temp_var = guess_top_n(
+        question=[question_sentence], params=params, max=3, n=1)
+    if(temp_var[0][1] < threshold_buzz):
         return "Buzzer never crosses the threshold", False
     store_index = index_of_bin_search
     max_index = index_of_bin_search - 1
@@ -38,8 +40,9 @@ def buzz(question, min_index=5):
     while max_index >= min_index:
         index_of_bin_search = (max_index+min_index)//2
         question_sentence = " ".join(temp_word_array[:index_of_bin_search])
-        temp_var = guess_top_n(question = [question_sentence], params = params, max = 3, n = 1)
-        if (temp_var[0][1]>threshold_buzz):
+        temp_var = guess_top_n(
+            question=[question_sentence], params=params, max=3, n=1)
+        if (temp_var[0][1] > threshold_buzz):
             max_index = index_of_bin_search-1
             store_index = index_of_bin_search
         else:
@@ -56,17 +59,20 @@ def buzz_full_question():
     start = time.time()
     buzzer_string, flag = buzz(question)
     end = time.time()
+    buzz_word = []
     # print(end - start)
     print("----TIME (s) : /binary_search_based_buzzer/buzz_full_question---", end - start)
 
     start = time.time()
     if(flag):
         importance_sentence = get_importance_of_each_sentence(buzzer_string)
+        buzzer_last_word=buzzer_string[-10:-1]
+        buzz_word.append(buzzer_last_word)
         buzzer_string = buzzer_string + " ||BUZZ||"
     else:
-        
+
         importance_sentence = get_importance_of_each_sentence(question)
     end = time.time()
     # print(end - start)
     print("----TIME (s) : /binary_search_based_buzzer/get_importance_sentence---", end - start)
-    return jsonify({"buzz": buzzer_string, "importance": importance_sentence})
+    return jsonify({"buzz": buzzer_string, "buzz_word": buzz_word, "importance": importance_sentence})
