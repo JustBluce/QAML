edf0c0748b228d47adf1d0ec24c07421294316b6
raj_shah <rajsanjayshah@gmail.com>
2021-07-21 19:27:21 +0530

    Bug correction transformers.logging

--- Flask/app/import_libraries.py
+++ Flask/app/import_libraries.py
@@ -54,4 +54,4 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 # nltk.download('punkt') #Download once
 from transformers import logging
 
-logging.set_verbosity_critical()
+logging.set_verbosity_error()
