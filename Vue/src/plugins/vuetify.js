c5278f35ea05e0c30d0884ea952802bea7047d6e
GeneralPoxter <generalpoxter@gmail.com>
2021-07-22 17:37:11 -0400

    hacky fix to perplexing vuetify tab problem

--- Vue/src/plugins/vuetify.js
+++ Vue/src/plugins/vuetify.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import { light, dark} from './theme';
+import { light, dark } from './theme';
 
 import '@mdi/font/css/materialdesignicons.css';
 
@@ -9,17 +9,11 @@ Vue.use(Vuetify);
 
 const opts = {
 	theme: {
-    dark: false,
-		themes: {
-			light, dark
-		}
+		dark: false,
+		themes: { light, dark },
+		options: { customProperties: true }
 	},
-  icons: {
-    iconfont: 'mdi',
-  },
-	options: {
-		customProperties: true
-	}
+	icons: { iconfont: 'mdi' }
 };
 
 export default new Vuetify(opts);
