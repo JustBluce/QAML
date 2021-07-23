bca3f3ecef0d44ef540f288c05caaa988187a296
GeneralPoxter <generalpoxter@gmail.com>
2021-07-23 09:26:18 -0400

    remove Vue Material dependency

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -20,11 +20,6 @@ import App from './App';
 import store from './store';
 import router from './router';
 
-
-import VueMaterial from 'vue-material';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default.css';
-
 import '@/icons'; // icon
 import '@/permission'; // permission control
 window._ = require('lodash');
@@ -43,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 Vue.use(ElementUI, { locale });
-Vue.use(VueAxios, axios, VueMaterial);
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
