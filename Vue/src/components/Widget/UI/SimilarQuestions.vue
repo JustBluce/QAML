f1ac4bc252c71c179ad10f36d90d3eaef1b8abe0
GeneralPoxter <generalpoxter@gmail.com>
2021-07-28 12:26:26 -0400

    Update stylings for widget UIs

--- Vue/src/components/Widget/UI/SimilarQuestions.vue
+++ Vue/src/components/Widget/UI/SimilarQuestions.vue
@@ -3,35 +3,25 @@ Developers: Atith Gandhi, Raj Shah and Jason Liu
 -->
 
 <template>
-  <div class="similar-container">
-    <div v-for="(val, index) in getTop5_similar_questions[1]" :key="index">
-      <div class="tab__header">
-        <a
-          href="#"
-          class="
-            tab__link
-            p-4
-            block
-            bg-blue-dark
-            hover:bg-blue-darker
-            no-underline
-            text-white
-            border-b-2 border-white
-            flex
-            justify-between
-          "
-          @click="toggle(index)"
-        >
-          {{ index + 1 }}.{{ val["answer"] }}
-          <span class="down-Arrow" v-show="!active[index]">&#9660;</span>
-          <span class="up-Arrow" v-show="active[index]">&#9650;</span>
-        </a>
-      </div>
-      <div class="tab__content p-2" v-show="active[index]">
-        <br />
-        {{ val["text"] }}
-      </div>
-    </div>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="similar_questions"
+      :expanded.sync="expanded"
+      item-key="id"
+      show-expand
+      hide-default-header
+      hide-default-footer
+      class="elevation-2 background"
+    >
+      <template v-slot:expanded-item="{ headers, item }">
+        <td :colspan="headers.length">
+          <span :style="{ color: $vuetify.theme.currentTheme.primary }">{{
+            item.text
+          }}</span>
+        </td>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
@@ -44,7 +34,11 @@ export default {
   },
   data() {
     return {
-      active: [false, false, false, false, false],
+      expanded: [],
+      headers: [
+        { text: "Answer", value: "answer" },
+        { text: "", value: "data-table-expand", align: "right" },
+      ],
     };
   },
   computed: {
@@ -54,28 +48,14 @@ export default {
     country_representation() {
       return this.qa.country_representation;
     },
-    getTop5_similar_questions() {
-      return this.qa.top5_similar_questions;
-    },
-  },
-  methods: {
-    toggle(index) {
-      this.active[index] = !this.active[index];
-      this.active = [...this.active];
-      console.log(index, this.active[index]);
+    similar_questions() {
+      if (this.qa.top5_similar_questions[1]) {
+        return this.qa.top5_similar_questions[1].map((question, index) =>
+          Object.assign(question, { id: index })
+        );
+      }
+      return [];
     },
   },
 };
 </script>
-
-<style scoped>
-.similar-container {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-}
-
-.container {
-  cursor: default;
-}
-</style>
