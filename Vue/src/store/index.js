269cc5a0e2ab75c83b706fe2ab9319012bec509e
GeneralPoxter <generalpoxter@gmail.com>
2021-08-13 12:40:19 -0400

    comment out store user

--- Vue/src/store/index.js
+++ Vue/src/store/index.js
@@ -121,7 +121,7 @@ const store = new Vuex.Store({
 	},
 	actions: {
 		
-		fetchUser({ commit }, user) {
+		/* fetchUser({ commit }, user) {
 			commit('SET_LOGGED_IN', user !== null);
 			if (user) {
 				commit('SET_USER', {
@@ -134,7 +134,7 @@ const store = new Vuex.Store({
 			} else {
 				commit('SET_USER', null);
 			}
-		}
+		} */
 	}
 });
 
