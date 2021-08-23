cef80d83e4637f9c9f799bb75691f01f0116df0e
GeneralPoxter <generalpoxter@gmail.com>
2021-08-22 22:10:47 -0400

    color themeing by widget

--- Vue/src/plugins/theme.js
+++ Vue/src/plugins/theme.js
@@ -5,7 +5,13 @@ export const light = {
 	background: colors.grey.lighten5,
 	sidebar: colors.blue.darken1,
 	red: colors.red.lighten2,
-	green: colors.green
+	green: colors.green,
+	Timer: colors.red.accent2,
+	Buzzer: colors.orange.accent2,
+	MachineGuesses: colors.green.accent2,
+	Pronunciation: colors.cyan.accent2,
+	CountryRepresentation: colors.purple.accent2,
+	SimilarQuestions: colors.deepPurple.accent2
 };
 
 export const dark = {
@@ -13,5 +19,11 @@ export const dark = {
 	background: '#303030',
 	sidebar: colors.blue.darken4,
 	red: colors.red.darken2,
-	green: colors.green
+	green: colors.green,
+	Timer: colors.red.accent3,
+	Buzzer: colors.orange.accent3,
+	MachineGuesses: colors.green.accent3,
+	Pronunciation: colors.cyan.accent3,
+	CountryRepresentation: colors.purple.accent3,
+	SimilarQuestions: colors.deepPurple.accent3
 };
