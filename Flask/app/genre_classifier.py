bd43398907802c1f9df94e4efb99964e375bcb08
raj_shah <rajsanjayshah@gmail.com>
2021-08-25 00:18:49 -0400

    Test

--- Flask/app/genre_classifier.py
+++ Flask/app/genre_classifier.py
@@ -59,7 +59,7 @@ def genre_data():
     if u_id is None:
         u_id = '0'
     
-    sql="SELECT Genre, count(*) from QA.Question  where UserId LIKE \'" + u_id  + "'\ group by Genre"
+    sql="SELECT Genre, count(*) from QA.Question  where UserId LIKE \'" + u_id  + "\' group by Genre"
     result_sql = db.session.execute(sql)
     result_sql = result_sql.fetchall()
     
